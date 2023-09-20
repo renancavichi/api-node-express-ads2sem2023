c68ba90fbe4925c16d6890c3066de63d710989f0
Renan Cavichi <renancavichi@gmail.com>
2023-09-19 21:57:45 -0300

    change delete user response

--- src/controllers/user/deleteUser.js
+++ src/controllers/user/deleteUser.js
@@ -5,13 +5,13 @@ const deleteUser = async (req, res)=>{
         const id = req.body.id
         const [result] = await user.remove(id)
         if(result.affectedRows === 1) {
-            res.status(200).json({message: `User id: ${id} Deleted Successfully!`})
+            res.status(200).json({success: `User id: ${id} Deleted Successfully!`})
         }else{
-            res.status(404).json({message: `User id: ${id} Not Found!`})
+            res.status(404).json({error: `User id: ${id} Not Found!`})
         }
     } catch(err) {
         console.log(err)
-        res.status(500).json({message: 'Server Error'})
+        res.status(500).json({error: 'Server Error'})
     }
 }
 
